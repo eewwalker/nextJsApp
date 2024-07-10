73152220c305e25b268592b0d0631ff5af4bd44b
Emily Walker <ewalker415@gmail.com>
2024-07-10 10:54:07 -0700

    navbar and routes

---
+++ src/app/members/page.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+
+
+const MembersPage = () => {
+  return (
+    <div>
+        <h3 className="text-3xl">This is members page</h3>
+        <Link href='/'>Go back home</Link>
+    </div>
+  )
+}
+export default MembersPage
---
+++ src/components/navbar/TopNav.tsx
@@ -0,0 +1,38 @@
+import { Button, Link, Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@nextui-org/react";
+import { GiAllSeeingEye } from "react-icons/gi";
+import NavLink from "./NavLink";
+
+
+const TopNav = () => {
+  return (
+   <Navbar
+        maxWidth="xl"
+        className="bg-gradient-to-r from-slate-400 to-slate-600"
+        classNames={{
+            item:[
+                'text-xl',
+                'text-white',
+                'uppercase',
+                'data-[active=true]:text-yellow-200'
+            ]
+        }}>
+    <NavbarBrand as={Link} href='/'>
+        <GiAllSeeingEye size={40} className="text-gray-200"/>
+        <div className="font-bold-text-3xl flex">
+            <span className="text-gray-900">Next</span>
+            <span className="text-gray-200">Match</span>
+        </div>
+    </NavbarBrand>
+    <NavbarContent justify='center'>
+        <NavLink href={'/members'} label={'Matches'}/>
+        <NavLink href={'/lists'} label={'Lists'}/>
+        <NavLink href={'/messages'} label={'Messages'}/>
+    </NavbarContent>
+    <NavbarContent justify="end">
+        <Button as={Link} href="/login" variant='bordered' className="text-white">Login</Button>
+        <Button as={Link} href="/register" variant='bordered' className="text-white">Register</Button>
+    </NavbarContent>
+   </Navbar>
+  )
+}
+export default TopNav
