03d0ce1a2b06463f8f6a0173fbddb1bf522313bc
Emily Walker <ewalker415@gmail.com>
2024-07-30 13:47:22 -0700

    add auth actions.ts signInUser, registerUser, getUserByEmail, getUserById

--- src/app/actions/authActions.ts
+++ src/app/actions/authActions.ts
@@ -1,36 +1,84 @@
 'use server';
 
+import { signIn } from "@/auth";
 import { prisma } from "@/lib/prisma";
+import { LoginSchema } from "@/lib/schemas/loginSchema";
 import { registerSchema, RegisterSchema } from "@/lib/schemas/registerSchema";
-import bcrypt from 'bcryptjs'
+import { ActionResult } from "@/types";
+import { User } from "@prisma/client";
+import bcrypt from 'bcryptjs';
+import { AuthError } from "next-auth";
+import { redirect } from "next/dist/server/api-utils";
 
-export async function registerUser(data: RegisterSchema) {
-  const validated = registerSchema.safeParse(data);
 
-  if(!validated.success) {
-    return {error: validated.error.errors}
-  }
-
-  const {name, email, password} = validated.data;
-
-  //10 => COST FACTOR number of rounds of hashing for password
-    //better security also takes more time to hash
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  const existingUser = await prisma.user.findUnique({
-    where:{email}
-
-  })
-
-  if(existingUser) {
-    return {error: 'User already exists'}
-  }
-
-  return prisma.user.create({
-    data: {
-      name,
-      email,
-      passwordHash: hashedPassword
+export async function signInUser(data: LoginSchema): Promise<ActionResult<string>>{
+  try {
+    const result = await signIn('credentials', {
+      email: data.email,
+      password: data.password,
+      redirect: false
+    })
+    console.log(result)
+    return {status: 'success', data: 'Logged In'}
+  } catch (error) {
+    console.log(error)
+    if(error instanceof AuthError) {
+      switch(error.type) {
+        case 'CredentialsSignin':
+          return {status: 'error', error: 'Invalid Credentials'}
+          default:
+            return {status: 'error', error: 'Something went wrong'}
+      }
+    } else {
+      return {status: 'error', error: 'Something else went wrong' }
     }
-  })
+  }
+}
+
+export async function registerUser(data: RegisterSchema):Promise<ActionResult<User>> {
+  try {
+    const validated = registerSchema.safeParse(data);
+
+    if (!validated.success) {
+      return { status: 'error', error: validated.error.errors };
+    }
+
+    const { name, email, password } = validated.data;
+
+    //10 => COST FACTOR number of rounds of hashing for password
+    //better security also takes more time to hash
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const existingUser = await prisma.user.findUnique({
+      where: { email }
+
+    });
+    if (existingUser) {
+      return { status: 'error', error: 'User already exists' };
+    }
+
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        passwordHash: hashedPassword
+      }
+    });
+
+    return {status: 'success', data: user};
+
+  } catch (e: any) {
+    console.log(e);
+    return {status: 'error', error: e.message};
+  }
+}
+
+export async function getUserByEmail(email:string) {
+  return prisma.user.findUnique({where: {email}});
+
+}
+
+export async function getUserById(id:string) {
+  return prisma.user.findUnique({where: {id}});
+
 }
